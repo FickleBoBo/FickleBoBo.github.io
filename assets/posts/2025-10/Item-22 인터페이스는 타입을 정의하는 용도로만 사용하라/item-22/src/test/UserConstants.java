a6a230db9a748694769e8abacf69e1ca628db1da
FickleBoBo <yukmw0704@gmail.com>
2025-11-20 18:58:25 +0900

    feat: Effective Java Item-22

---
+++ assets/posts/2025-10/Item-22 인터페이스는 타입을 정의하는 용도로만 사용하라/item-22/src/test/UserConstants.java
@@ -0,0 +1,16 @@
+package test;
+
+public class UserConstants implements PhysicalConstants {
+
+    public double getAN() {
+        return AVOGADROS_NUMBER;
+    }
+
+    public double getBC() {
+        return BOLTZMANN_CONSTANT;
+    }
+
+    public double getEM() {
+        return ELECTRON_MASS;
+    }
+}
